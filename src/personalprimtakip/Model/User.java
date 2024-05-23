9d5ed250dd887541be10e226c5342187e9a5daf1
emiiryv <111075935+emiiryv@users.noreply.github.com>
2024-05-24 02:48:29 +0300

    step-1.4
    ata searching developments

--- src/personalprimtakip/Model/User.java
+++ src/personalprimtakip/Model/User.java
@@ -93,7 +93,7 @@ public class User {
         String query = "INSERT INTO public.user (name, uname, pass, type) VALUES (?, ?, ?, ?::user_type)";
         User findUser = User.getFetch(uname);
         if (findUser != null){
-            Helper.showMsg("Bu kullanıcı adı daha önceden eklenmiş.Lütfen farklı bir kullanıcı adı giriniz.");
+            Helper.showMsg("Bu kullanıcı adı daha önceden eklenmiş. Lütfen farklı bir kullanıcı adı giriniz.");
             return false;
         }
         try {
@@ -121,7 +121,7 @@ public class User {
         String query = "SELECT * FROM public.user WHERE uname = ?";
         try {
             PreparedStatement pr = DBConnector.getInstance().prepareStatement(query);
-            pr.setString(1,uname);
+            pr.setString(1, uname);
             ResultSet rs = pr.executeQuery();
             while (rs.next()){
                 obj = new User();
@@ -141,7 +141,7 @@ public class User {
         String query = "DELETE FROM public.user WHERE id = ?";
         try {
             PreparedStatement pr = DBConnector.getInstance().prepareStatement(query);
-            pr.setInt(1,id);
+            pr.setInt(1, id);
             return pr.executeUpdate() != -1;
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -149,6 +149,55 @@ public class User {
         return true;
     }
 
+    public static boolean update(int id, String name, String uname, String pass, String type){
+        String query = "UPDATE public.user SET name = ?, uname = ?, pass = ?, type = ? WHERE id = ?";
+        User findUser = User.getFetch(uname);
+        if (findUser != null && findUser.getId() != id){
+            Helper.showMsg("Bu kullanıcı adı daha önce eklenmiş.");
+            return false;
+        }
+        try {
+            PreparedStatement pr = DBConnector.getInstance().prepareStatement(query);
+            pr.setString(1, name);
+            pr.setString(2, uname);
+            pr.setString(3, pass);
+            pr.setString(4, type);
+            pr.setInt(5, id);
+            return pr.executeUpdate() != -1;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return true;
+    }
+
+    public static ArrayList<User> searchUserList(String query) {
+        ArrayList<User> userList = new ArrayList<>();
+        User obj;
+        try {
+            Statement st = DBConnector.getInstance().createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                obj = new User();
+                obj.setId(rs.getInt("id"));
+                obj.setName(rs.getString("name"));
+                obj.setUname(rs.getString("uname"));
+                obj.setPass(rs.getString("pass"));
+                obj.setType(rs.getString("type"));
+                userList.add(obj);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return userList;
+    }
+
+    public static String searchQuery(String name, String uname, String type){
+        String query = "SELECT * FROM public.user WHERE uname LIKE '%" + uname + "%' AND name LIKE '%" + name + "%'";
+        if (!type.isEmpty()){
+            query += " AND type = '" + type + "'";
+        }
+
+        System.out.println(query);
+        return query;
+    }
 }
-
-
--- src/personalprimtakip/View/OperatorGUI.java
+++ src/personalprimtakip/View/OperatorGUI.java
@@ -6,11 +6,11 @@ import personalprimtakip.Model.Operator;
 import personalprimtakip.Model.User;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
+import javax.swing.event.TableModelEvent;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class OperatorGUI extends JFrame {
     private JPanel wrapper;
@@ -29,6 +29,10 @@ public class OperatorGUI extends JFrame {
     private JButton btn_user_add;
     private JTextField fld_user_id;
     private JButton btn_user_delete;
+    private JTextField fld_sh_user_name;
+    private JTextField fld_sh_user_uname;
+    private JComboBox cmb_sh_user_type;
+    private JButton btn_user_sh;
     private DefaultTableModel mdl_user_list;
     private Object[] row_user_list;
 
@@ -53,7 +57,7 @@ public class OperatorGUI extends JFrame {
                 if (column == 0){
                     return false;
                 }
-                return super.isCellEditable(row,column);
+                return super.isCellEditable(row, column);
             }
         };
         Object[] col_user_list = {"ID", "Ad", "Kullanıcı Adı", "Şifre", "Üyelik Tipi"};
@@ -65,15 +69,28 @@ public class OperatorGUI extends JFrame {
         tbl_user_list.setModel(mdl_user_list);
         tbl_user_list.getTableHeader().setReorderingAllowed(false);
 
-
         tbl_user_list.getSelectionModel().addListSelectionListener(e -> {
             try {
-                String select_user_id = tbl_user_list.getValueAt(tbl_user_list.getSelectedRow(),0).toString();
+                String select_user_id = tbl_user_list.getValueAt(tbl_user_list.getSelectedRow(), 0).toString();
                 fld_user_id.setText(select_user_id);
-            }catch (Exception exception){
+            } catch (Exception exception){
             }
         });
 
+        tbl_user_list.getModel().addTableModelListener(e -> {
+            if (e.getType() == TableModelEvent.UPDATE){
+                int user_id = Integer.parseInt(tbl_user_list.getValueAt(tbl_user_list.getSelectedRow(), 0).toString());
+                String user_name = tbl_user_list.getValueAt(tbl_user_list.getSelectedRow(), 1).toString();
+                String user_uname = tbl_user_list.getValueAt(tbl_user_list.getSelectedRow(), 2).toString();
+                String user_pass = tbl_user_list.getValueAt(tbl_user_list.getSelectedRow(), 3).toString();
+                String user_type = tbl_user_list.getValueAt(tbl_user_list.getSelectedRow(), 4).toString();
+
+                if (User.update(user_id, user_name, user_uname, user_pass, user_type)){
+                    Helper.showMsg("done");
+                }
+                loadUserModel();
+            }
+        });
 
         btn_user_add.addActionListener(e -> {
             if (Helper.isFieldEmpty(fld_user_name) || Helper.isFieldEmpty(fld_user_uname) || Helper.isFieldEmpty(fld_user_pass)) {
@@ -92,6 +109,7 @@ public class OperatorGUI extends JFrame {
                 }
             }
         });
+
         btn_user_delete.addActionListener(e -> {
             if (Helper.isFieldEmpty(fld_user_id)){
                 Helper.showMsg("fill");
@@ -105,6 +123,18 @@ public class OperatorGUI extends JFrame {
                 }
             }
         });
+
+        btn_user_sh.addActionListener(e -> {
+            String name = fld_sh_user_name.getText();
+            String uname = fld_sh_user_uname.getText();
+            String type = cmb_sh_user_type.getSelectedItem().toString();
+            String query = User.searchQuery(name, uname, type);
+            loadUserModel(User.searchUserList(query));
+        });
+
+        btn_logout.addActionListener(e -> {
+            dispose();
+        });
     }
 
     public void loadUserModel() {
@@ -112,6 +142,21 @@ public class OperatorGUI extends JFrame {
         clearModel.setRowCount(0);
 
         for (User obj : User.getList()) {
+            int i = 0;
+            row_user_list[i++] = obj.getId();
+            row_user_list[i++] = obj.getName();
+            row_user_list[i++] = obj.getUname();
+            row_user_list[i++] = obj.getPass();
+            row_user_list[i++] = obj.getType();
+            mdl_user_list.addRow(row_user_list);
+        }
+    }
+
+    public void loadUserModel(ArrayList<User> list) {
+        DefaultTableModel clearModel = (DefaultTableModel) tbl_user_list.getModel();
+        clearModel.setRowCount(0);
+
+        for (User obj : list) {
             int i = 0;
             row_user_list[i++] = obj.getId();
             row_user_list[i++] = obj.getName();
@@ -131,5 +176,9 @@ public class OperatorGUI extends JFrame {
         op.setUname("emo");
 
         OperatorGUI opGUI = new OperatorGUI(op);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
